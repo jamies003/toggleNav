4a874c40b6724d4bd09ad9db208d51edeaafabff
Jamie Stewart <stwjme@gmail.com>
2020-04-09 20:34:55 -0400

    AddToggleNavFunction

---
+++ toggleNav.js
@@ -0,0 +1,9 @@
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarTitle = document.querySelector(".nav-sidebar span");
+    let getSidebarLinks = document.querySelectorAll(".nav-sidebar a");
+
+}
